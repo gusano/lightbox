1668ba17e821441be49acde600a182d9bedd19d7
Yvan Volochine <yvan.github@gmail.com>
2017-09-02 21:11:05 +0200

    new build

--- dist/ekko-lightbox.js
+++ dist/ekko-lightbox.js
@@ -615,7 +615,7 @@ var Lightbox = (function ($) {
 				}
 
 				this._$lightboxContainer.css('height', maxHeight);
-				this._$modalDialog.css('width', 'auto').css('maxWidth', width);
+				this._$modalDialog.css('flex', '1').css('maxWidth', width);
 
 				var modal = this._$modal.data('bs.modal');
 				if (modal) {
